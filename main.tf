20aec2921b70167728c2f2250cc5cf9520bd0ad4
Hemanth Sai Balasada <balasadahemanth@gmail.com>
2025-02-19 16:04:10 -0500

    updated CI/cd

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # Set your desired AWS region
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-awesome-bucket"
-  acl    = "private"
+  bucket = "my-awesome-bucket"  # Replace with a globally unique bucket name
+  # Remove the deprecated ACL argument here
 }
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  acl    = "private"  # Set ACL to private
+}
+
+# Optional: you can add other resources such as IAM roles, policies, etc., below
