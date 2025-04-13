0400b3c43b4e42ca25f8d6b7e5884d6aada41266
Hemanth Sai Balasada <balasadahemanth@gmail.com>
2025-04-13 16:20:29 -0400

    zip

--- main.tf
+++ main.tf
@@ -2,38 +2,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Variable to store the Meta Graph API Access Token
 variable "meta_graph_api_access_token" {
   description = "Meta Graph API Access Token"
   type        = string
 }
 
-resource "aws_lambda_function" "meta_graph_lambda" {
-  function_name = "MetaGraphAPIToS3"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.lambda_handler"
-  runtime       = "python3.8"
-  timeout       = 15
-  filename      = "function.zip"  # Path to your lambda zip file
-
-  environment {
-    variables = {
-      META_GRAPH_API_ACCESS_TOKEN = var.meta_graph_api_access_token  # Environment variable from GitHub secrets
-      S3_BUCKET                  = aws_s3_bucket.meta_graph_s3.bucket
-    }
-  }
-
-  depends_on = [aws_iam_role.lambda_role]
-}
-
+# Create an S3 bucket to store engagement data
 resource "aws_s3_bucket" "meta_graph_s3" {
   bucket = "meta-graph-api-data"
 }
 
+# IAM Role for Lambda execution
 resource "aws_iam_role" "lambda_role" {
   name               = "lambda_execution_role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
 }
 
+# Assume role policy for Lambda to access the IAM role
 resource "aws_iam_policy_document" "lambda_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -44,6 +30,7 @@ resource "aws_iam_policy_document" "lambda_assume_role_policy" {
   }
 }
 
+# IAM Policy for Lambda to allow interaction with S3 and CloudWatch
 resource "aws_iam_role_policy" "lambda_policy" {
   name   = "lambda_policy"
   role   = aws_iam_role.lambda_role.id
@@ -62,4 +49,34 @@ resource "aws_iam_role_policy" "lambda_policy" {
       }
     ]
   })
+}
+
+# Lambda Function Resource
+resource "aws_lambda_function" "meta_graph_lambda" {
+  function_name = "MetaGraphAPIToS3"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"  # Make sure this matches your Python function
+  runtime       = "python3.8"
+  timeout       = 15
+  filename      = "function.zip"  # Path to your lambda zip file
+
+  environment {
+    variables = {
+      META_GRAPH_API_ACCESS_TOKEN = var.meta_graph_api_access_token  # Pass the GitHub Secrets to Lambda
+      S3_BUCKET                  = aws_s3_bucket.meta_graph_s3.bucket
+    }
+  }
+
+  depends_on = [aws_iam_role.lambda_role]
+}
+
+# Data source to fetch the IAM policy document for Lambda execution
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
 }
