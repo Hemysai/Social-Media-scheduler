d4ca4d541f4cc3a116672dd508da36dfe52e0a39
Hemanth Sai Balasada <balasadahemanth@gmail.com>
2025-05-08 13:51:01 -0400

    ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 AMI for us-east-1
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "GitHubEC2"
+  }
 }
