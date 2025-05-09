f7f675686fecf1a3653885a3248c604579d71542
Hemanth Sai Balasada <balasadahemanth@gmail.com>
2025-05-09 15:01:11 -0400

    changed cari

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,15 @@ variable "region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "project" {
+  type = string
+}
