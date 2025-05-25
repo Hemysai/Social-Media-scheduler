fe54e22c0483eecb1a7e6c9499a4c40a871a43e3
Hemanth Sai Balasada <balasadahemanth@gmail.com>
2025-05-24 23:04:58 -0400

    backend clear

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-hemanth"
-    key            = "infra/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
-
 provider "aws" {
   region = var.aws_region
 }
