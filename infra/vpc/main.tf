60be4d1aef35466f30578f83f561f280e98a7f35
Hemanth Sai Balasada <balasadahemanth@gmail.com>
2025-05-26 14:43:57 -0400

    ma pv

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "private" {
   count = 2
-  vpc_id = aws_vpc.main
+  vpc_id = aws_vpc.main.id
   cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index+10)
   availability_zone = element(var.azs, count.index)
   map_public_ip_on_launch = false
