76be5973faaae5ce23768dd259ac6c7a84a43e0c
Hemanth Sai Balasada <balasadahemanth@gmail.com>
2025-05-26 22:52:12 -0400

    main priv

--- infra/main.tf
+++ infra/main.tf
@@ -9,8 +9,8 @@ module "rds" {
   source            = "./rds"
   project           = var.project
   vpc_id            = module.vpc.vpc_id
-  subnet_ids        = module.vpc.public_subnet_ids
   private_subnet_ids = module.vpc.private_subnet_ids
+  subnet_ids        = module.vpc.public_subnet_ids
   availability_zone = var.azs[0]
   rds_secret_name   = var.rds_secret_name
 }
