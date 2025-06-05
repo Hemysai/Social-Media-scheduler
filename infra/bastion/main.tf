890407cae79b0187112b87bef17423c0901c94a2
Hemanth Sai Balasada <balasadahemanth@gmail.com>
2025-06-05 15:13:08 -0400

    copy

--- infra/bastion/main.tf
+++ infra/bastion/main.tf
@@ -19,17 +19,19 @@ resource "aws_security_group" "bastion" {
 }
 
 resource "aws_iam_role" "bastion_role" {
-  name= "bastion-iam-role"
+  name = "bastion-iam-role"
 
   assume_role_policy = jsonencode({
-    version = "2012-10-17"
-    statement = [{
-      Effect = "Allow"
-      principal = {
-        service = "ec2.amazon.com"
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
       }
-      Action = "sts.Assumerole"
-    }]
+    ]
   })
 }
 
@@ -37,27 +39,17 @@ resource "aws_iam_policy" "bastion_secrets_policy" {
   name = "bastion-secrets-read-policy"
 
   policy = jsonencode({
-    version = "2012-10-17"
-    statement = [
+    Version = "2012-10-17",
+    Statement = [
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = [
-          "secretmanager:GetSecretValue"
-        ]
-        resource = var.secrets_arn
+          "secretsmanager:GetSecretValue"
+        ],
+        Resource = var.secrets_arn
       }
     ]
   })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_secrets_policy" {
-  role = aws_iam_role.bastion_role.name
-  policy_arn = aws_iam_policy.bastion_secrets_policy.arn
-}
-
-resource "aws_iam_instance_profile" "bastion_instance_profile" {
-  name = "bastion_instance_profile"
-  role = aws_iam_role.bastion_role.name
 }
 
 resource "aws_instance" "bastion" {
@@ -65,7 +57,7 @@ resource "aws_instance" "bastion" {
   instance_type = "t3.micro"
   subnet_id     = var.subnet_id
   key_name      = var.key_name
-  security_groups = [aws_security_group.bastion.id]
+  vpc_security_group_ids = [aws_security_group.bastion.id]
   iam_instance_profile   = aws_iam_instance_profile.bastion_instance_profile.name
 
   tags = {
