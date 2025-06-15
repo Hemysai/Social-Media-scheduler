17a390ebb2c37782687ec6a44c442ee46186f3b3
Hemanth Sai Balasada <balasadahemanth@gmail.com>
2025-06-15 15:47:28 -0400

    endpoint clear

--- infra/ssm/outputs.tf
+++ infra/ssm/outputs.tf
@@ -2,19 +2,19 @@ output "ec2_security_group_id" {
   value = aws_security_group.ec2.id
 }
 
-output "ec2_instance_id" {
-  value = aws_instance.ssm_ec2.id
-}
+# output "ec2_instance_id" {
+#   value = aws_instance.ssm_ec2.id
+# }
 
 
-output "ssm_vpc_endpoint_id" {
-  value = aws_vpc_endpoint.ssm.id
-}
+# output "ssm_vpc_endpoint_id" {
+#   value = aws_vpc_endpoint.ssm.id
+# }
 
-output "ssm_messages_vpc_endpoint_id" {
-  value = aws_vpc_endpoint.ssmmessages.id
-}
+# output "ssm_messages_vpc_endpoint_id" {
+#   value = aws_vpc_endpoint.ssmmessages.id
+# }
 
-output "ec2_messages_vpc_endpoint_id" {
-  value = aws_vpc_endpoint.ec2messages.id
-}
+# output "ec2_messages_vpc_endpoint_id" {
+#   value = aws_vpc_endpoint.ec2messages.id
+# }
--- infra/ssm/ssm_endpoints.tf
+++ infra/ssm/ssm_endpoints.tf
@@ -1,65 +1,65 @@
-resource "aws_vpc_endpoint" "ssm" {
-  vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.${var.region}.ssm"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = var.private_subnet_ids
-  security_group_ids = [aws_security_group.endpoint_sg.id]
+# resource "aws_vpc_endpoint" "ssm" {
+#   vpc_id            = var.vpc_id
+#   service_name      = "com.amazonaws.${var.region}.ssm"
+#   vpc_endpoint_type = "Interface"
+#   subnet_ids        = var.private_subnet_ids
+#   security_group_ids = [aws_security_group.endpoint_sg.id]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = {
-    Name = "${var.project}-ssm-endpoint"
-  }
-}
+#   tags = {
+#     Name = "${var.project}-ssm-endpoint"
+#   }
+# }
 
-resource "aws_vpc_endpoint" "ssmmessages" {
-  vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.${var.region}.ssmmessages"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = var.private_subnet_ids
-  security_group_ids = [aws_security_group.endpoint_sg.id]
+# resource "aws_vpc_endpoint" "ssmmessages" {
+#   vpc_id            = var.vpc_id
+#   service_name      = "com.amazonaws.${var.region}.ssmmessages"
+#   vpc_endpoint_type = "Interface"
+#   subnet_ids        = var.private_subnet_ids
+#   security_group_ids = [aws_security_group.endpoint_sg.id]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = {
-    Name = "${var.project}-ssmmessages-endpoint"
-  }
-}
+#   tags = {
+#     Name = "${var.project}-ssmmessages-endpoint"
+#   }
+# }
 
-resource "aws_vpc_endpoint" "ec2messages" {
-  vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.${var.region}.ec2messages"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = var.private_subnet_ids
-  security_group_ids = [aws_security_group.endpoint_sg.id]
+# resource "aws_vpc_endpoint" "ec2messages" {
+#   vpc_id            = var.vpc_id
+#   service_name      = "com.amazonaws.${var.region}.ec2messages"
+#   vpc_endpoint_type = "Interface"
+#   subnet_ids        = var.private_subnet_ids
+#   security_group_ids = [aws_security_group.endpoint_sg.id]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = {
-    Name = "${var.project}-ec2messages-endpoint"
-  }
-}
+#   tags = {
+#     Name = "${var.project}-ec2messages-endpoint"
+#   }
+# }
 
-resource "aws_security_group" "endpoint_sg" {
-  name        = "${var.project}-endpoint-sg"
-  description = "Security group for VPC endpoints"
-  vpc_id      = var.vpc_id
+# resource "aws_security_group" "endpoint_sg" {
+#   name        = "${var.project}-endpoint-sg"
+#   description = "Security group for VPC endpoints"
+#   vpc_id      = var.vpc_id
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
-  }
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = [var.vpc_cidr]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "${var.project}-endpoint-sg"
-  }
-}
+#   tags = {
+#     Name = "${var.project}-endpoint-sg"
+#   }
+# }
